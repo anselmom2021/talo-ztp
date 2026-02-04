406219ea4d7620b12354d2d468749eb52798c0fc
anselmom2021 <marcoifanselmo@gmail.com>
2026-02-04 18:18:50 +0000

    Correcting a little bug on app.js

--- talos-ztp/web/public/app.js
+++ talos-ztp/web/public/app.js
@@ -29,11 +29,11 @@ function statusPill(status) {
 
 function escapeHtml(value) {
   return value
-    .replaceAll("&", "&amp;")
-    .replaceAll("<", "&lt;")
-    .replaceAll(">", "&gt;")
-    .replaceAll('"', "&quot;")
-    .replaceAll("'", "&#039;");
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
 }
 
 function nodeCard(node) {
